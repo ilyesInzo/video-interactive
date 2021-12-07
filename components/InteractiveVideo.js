fb970f18f275dbe01ae8e24d44770f5599d514b0
user <ilyes.mansour.3@gmail.com>
2021-12-07 21:40:58 +0100

    Design improvement

--- components/InteractiveVideo.js
+++ components/InteractiveVideo.js
@@ -5,7 +5,9 @@ import "video.js/dist/video-js.css";
 import videoStyle from "./videoContainer.module.scss"
 import 'videojs-disable-progress-bar/lib/DisableProgressBar'
 import { useEffect, useState } from 'react';
-import ParticleEffectButton from 'react-particle-effect-button'
+import {attacheParticule} from '../lib/particle'
+import {fullScreenByElementIdHandler, fullScreenByElementHandler} from '../lib/fullScreen'
+import {attachButtonToVideo} from '../lib/videoButton'
 //import anime from 'animejs/lib/anime.es.js';
 export default function InteractiveVideo() {
 
@@ -27,7 +29,6 @@ export default function InteractiveVideo() {
     console.log("tu as clicker sur " + choixX);
     setChoixX("choix3")
     setChoixY("choix4")
-    console.log(getPlayer());
     getPlayer().currentTime(30)
 
   }
@@ -36,7 +37,6 @@ export default function InteractiveVideo() {
     console.log("tu as clicker sur " + choixY);
     setChoixX("choix5")
     setChoixY("choix6")
-    console.log(getPlayer());
     getPlayer().currentTime(60)
   }
 
@@ -44,30 +44,12 @@ export default function InteractiveVideo() {
 
   useEffect(() => {
 
-    
-    /*
-      const videoJsOptions = { // lookup the options in the docs for more options
-        autoplay: false,
-        controls: false,
-        responsive: true,
-        fluid: true,
-      }
-    
-      // player is ready this will be executed
-      var player = videojs('myPlayer', videoJsOptions, (p) =>  {
-        videojs.log('Your player is ready!');
-    
-        // In this context, `this` is the player that was created by Video.js.
-        //this.play();
-      
-        // How about an event listener?
-        player.on('ended', function() {
-          draw()
-        });
-      });*/
-
     player = getPlayer();
 
+    attachButtonToVideo(player, fullScreenByElementHandler)
+    attacheParticule("choixX")
+    attacheParticule("choixY")
+    fullScreenByElementIdHandler("go_fullscreen")
     player.on('ready', () => {
       const bar = player.DisableProgressBar()
       bar.disable()
@@ -87,36 +69,29 @@ export default function InteractiveVideo() {
 
   ])
 
-  useEffect(() => {
-    console.log("log")
-  }, [width, height
-
-  ])
-
-
 
   return (
     <>
+<button id="go_fullscreen">Go FullScreen</button>
 
+      <div  className={videoStyle.outerContainer}>
+        <div id="container" className={videoStyle.innerContainer}>
 
-      <div className={videoStyle.outerContainer}>
-        <div className={videoStyle.innerContainer}>
+          <button id="choixX"  className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox}`} onClick={(e) => executeChoixX(e)}>{choixX}</button>
 
-          <button  className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox}`} onClick={(e) => executeChoixX(e)}>{choixX}</button>
+          <button id="choixY" className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox2}`} onClick={(e) => executeChoixY(e)}>{choixY}</button>
 
-          <button  className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox2}`} onClick={(e) => executeChoixY(e)}>{choixY}</button>
-
-          <button id="choixX" style={{display:"none"}} className={videoStyle.videoOverlay} onClick={(e) => executeChoixX(e)}>{choixX}</button>
-          <button id="choixY" style={{display:"none"}} className={videoStyle.videoOverlay2} onClick={(e) => executeChoixY(e)}>{choixY}</button>
+          <button  style={{display:"none"}} className={videoStyle.videoOverlay} onClick={(e) => executeChoixX(e)}>{choixX}</button>
+          <button  style={{display:"none"}} className={videoStyle.videoOverlay2} onClick={(e) => executeChoixY(e)}>{choixY}</button>
           <button id="ss" style={{display:"none"}} className={videoStyle.yellowbutton} 
  >{choixX}</button>
           <video id="myPlayer" className="video-js vjs-default-skin" width={width} height={height}
-            controls preload="none" poster='/hamhama.jpg' style={{ objectFit: "cover" }}
+            controls preload="none" poster='/hamhama.jpg' 
             data-setup='{ 
            "responsive":true,
            "fluid":true,
            "controlBar": {
-            "fullscreenToggle": true,
+            "fullscreenToggle": false,
             "currentTimeDisplay": false,
             "timeDivider": false,
             "durationDisplay": false,
@@ -129,37 +104,9 @@ export default function InteractiveVideo() {
           </video>
         </div>
       </div>
-      <ParticleEffectButton
-        hidden={false}
-      >
-          <button
-            className="particle-effect-button"
 
-          >
-            {"sffefesgz"}
-          </button>
-      </ParticleEffectButton>
 
-      <ParticleEffectButton
-            color="#121019"
-            type="triangle"
-            hidden={hidden}
-          >
-            <div
-              style={{
-                background: "#121019",
-                color: "#fff",
-                padding: "1.5rem 3rem",
-                border: "0",
-                borderRadius: 5,
-                cursor: "pointer",
-                fontSize: "1.2em"
-              }}
-              onClick={()=>{}}
-            >
-              Click Me
-            </div>
-          </ParticleEffectButton>
+
     </>
   )
 }
@@ -169,3 +116,25 @@ export default function InteractiveVideo() {
           <a href="#" className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate}`} onClick={(e) => executeChoixX(e)}>{choixX}</a>
         </div>
         */
+
+
+           /*
+      const videoJsOptions = { // lookup the options in the docs for more options
+        autoplay: false,
+        controls: false,
+        responsive: true,
+        fluid: true,
+      }
+    
+      // player is ready this will be executed
+      var player = videojs('myPlayer', videoJsOptions, (p) =>  {
+        videojs.log('Your player is ready!');
+    
+        // In this context, `this` is the player that was created by Video.js.
+        //this.play();
+      
+        // How about an event listener?
+        player.on('ended', function() {
+          draw()
+        });
+      });*/
