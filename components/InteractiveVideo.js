bfd3b30f56aa0382e7d560fc6572ac096e1f1d6a
user <ilyes.mansour.3@gmail.com>
2021-12-14 17:23:35 +0100

    start scenarios + spinner

--- components/InteractiveVideo.js
+++ components/InteractiveVideo.js
@@ -5,88 +5,179 @@ import "video.js/dist/video-js.css";
 import videoStyle from "./videoContainer.module.scss"
 import 'videojs-disable-progress-bar/lib/DisableProgressBar'
 import { useEffect, useState } from 'react';
-import {attacheParticule} from '../lib/particle'
-import {fullScreenByElementIdHandler, fullScreenByElementHandler} from '../lib/fullScreen'
-import {attachButtonToVideo} from '../lib/videoButton'
+import { attacheParticule, desintegrate, exploseElement } from '../lib/particle'
+import { displaySpinner, hideSpinner, reverseSpinner } from '../lib/spinner'
+import { fullScreenByElementIdHandler, fullScreenByElementHandler } from '../lib/fullScreen'
+import { attachButtonToVideo } from '../lib/videoButton'
+import { jsonToMap } from '../lib/ObjectMap'
+import setting from '../data/settings.json'
+//import {usersRepo} from '../lib/manageSettings'
 //import anime from 'animejs/lib/anime.es.js';
 export default function InteractiveVideo() {
+  
+
+  // normal enchainment
+  /*let scenarios = new Map([
+    ['choice1', 20],
+    ['choice2', 40],
+    ['choice3', 60],
+    ['choice4', 90],
+    ['choice5', 120],
+  ]
+  )*/
+  // preceision en seconde
+  const precison = 0.5;
+  let breackPointToScenarios = new Map()
 
   let player;
-  const currentChoix = "Choix0"
-  const [hidden, setHidden] = useState(false)
+  const currentChoix = {}
   const [choixX, setChoixX] = useState("Choix1")
   const [choixY, setChoixY] = useState("Choix2")
 
-  const [width, setWidth] = useState(640 + "px")
-  const [height, setHeight] = useState(267 + "px")
-
-  const draw = () => {
-    videojs.log('Awww...over so soon?!');
-    console.log("Hi");
-  }
-
   const executeChoixX = (e) => {
-    console.log("tu as clicker sur " + choixX);
-    setChoixX("choix3")
-    setChoixY("choix4")
+    //console.log("tu as clicker sur " + choixX);
+    loadAudio("clickSound")
     getPlayer().currentTime(30)
-
+    hideSpinner()
+    getChoiceButton("choixY").style.display = "none"
   }
 
   const executeChoixY = (e) => {
-    console.log("tu as clicker sur " + choixY);
-    setChoixX("choix5")
-    setChoixY("choix6")
+    //console.log("tu as clicker sur " + choixY);
+    loadAudio("clickSound")
     getPlayer().currentTime(60)
+    hideSpinner()
+    getChoiceButton("choixX").style.display = "none"
+  }
+
+  const runSpinner = (e) => {
+    displaySpinner()
+  }
+
+  const stopSpinner = (e) => {
+    hideSpinner()
+  }
+
+  const pauseSpinner = (e) => {
+    reverseSpinner()
+  }
+
+  const getChoiceButton = (buttonId) => {
+    return document.getElementById(buttonId)
+  }
+
+  const getAudioComponent = (audioId) => {
+    return document.getElementById(audioId)
+  }
+
+  const loadAudio = (audioId) => {
+    const currentAudio = getAudioComponent(audioId)
+    currentAudio.load()
+    currentAudio.play()
+  }
+
+  const stopAudio = (audioId) => {
+    const currentAudio = getAudioComponent(audioId)
+    currentAudio.pause()
+    currentAudio.currentTime = 0;
   }
 
   const getPlayer = () => videojs('myPlayer')
 
   useEffect(() => {
-
     player = getPlayer();
+    //desintegrate("choixX")
+    breackPointToScenarios = jsonToMap(setting)
+    /*console.log(breackPointToScenarios.get(10))*/
+    /*
+    runSpinner()
+    pauseSpinner()
+    */
 
-    attachButtonToVideo(player, fullScreenByElementHandler)
-    attacheParticule("choixX")
-    attacheParticule("choixY")
+    var choixXBtn = getChoiceButton("choixX");
+    var choixYBtn = getChoiceButton("choixY");
+    choixXBtn.style.display = "block"
+    choixYBtn.style.display = "block"
+    attacheParticule(choixXBtn)
+    attacheParticule(choixYBtn)
+    choixXBtn.style.display = "none"
+    choixYBtn.style.display = "none"
+
+
     fullScreenByElementIdHandler("go_fullscreen")
     player.on('ready', () => {
       const bar = player.DisableProgressBar()
       bar.disable()
+      attachButtonToVideo(player, fullScreenByElementHandler)
 
       player.on('ended', () => {
         console.log("ended")
       });
 
+      let executed = false
+
       player.on('timeupdate', () => {
         //console.log(player.currentTime());
+        //if()
+
+        let currentTime = player.currentTime()
+        breackPointToScenarios.forEach((value, key) => {
+          if (key < currentTime && currentTime < (key + precison)) {
+            console.log("errr");
+            setChoixX(value.choix1.text)
+            setChoixY(value.choix2.text)
+            var choixXBtn = getChoiceButton("choixX");
+            var choixYBtn = getChoiceButton("choixY");
+            choixXBtn.style.display = "block";
+            choixYBtn.style.display = "block";
+            displaySpinner(() => {
+
+              choixXBtn.style.display = "none";
+              choixYBtn.style.display = "none";
+
+            })
+          }
+        })
+
+
+      });
+
+      player.on("pause", () => {
+        console.log("pause");
+        pauseSpinner()
+      });
+
+      player.on("play", () => {
+        pauseSpinner()
+        console.log("play");
       });
 
     });
-
-
-  }, [
-
-  ])
-
+  }, [])
 
   return (
-    <>
-<button id="go_fullscreen">Go FullScreen</button>
 
-      <div  className={videoStyle.outerContainer}>
+    <div className={videoStyle.centerContant}>
+      
+      <audio id="clickSound" src="/audios/sound1.wav" type="audio/mpeg"></audio>
+      <button id="go_fullscreen" style={{ display: "none" }}>Go FullScreen</button>
+      <button onClick={(e) => runSpinner(e)}>Run Spinner</button>
+      <button onClick={(e) => stopSpinner(e)}>Stop Spinner</button>
+      <button onClick={(e) => pauseSpinner(e)}>Pause Spinner</button>
+      <div className={videoStyle.outerContainer}>
         <div id="container" className={videoStyle.innerContainer}>
 
-          <button id="choixX"  className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox}`} onClick={(e) => executeChoixX(e)}>{choixX}</button>
+          <button id="choixX" style={{ display: "none" }} className={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox}`} onClick={(e) => executeChoixX(e)}>{choixX}</button>
+          <button id="choixY" style={{ display: "none" }} className={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox2}`} onClick={(e) => executeChoixY(e)}>{choixY}</button>
+          
+          <div className={videoStyle.spinner} id="app"></div>
+          <button style={{ display: "none" }} className={videoStyle.videoOverlay} onClick={(e) => executeChoixX(e)}>{choixX}</button>
+          <button style={{ display: "none" }} className={videoStyle.videoOverlay2} onClick={(e) => executeChoixY(e)}>{choixY}</button>
 
-          <button id="choixY" className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox2}`} onClick={(e) => executeChoixY(e)}>{choixY}</button>
-
-          <button  style={{display:"none"}} className={videoStyle.videoOverlay} onClick={(e) => executeChoixX(e)}>{choixX}</button>
-          <button  style={{display:"none"}} className={videoStyle.videoOverlay2} onClick={(e) => executeChoixY(e)}>{choixY}</button>
-          <button id="ss" style={{display:"none"}} className={videoStyle.yellowbutton} 
- >{choixX}</button>
-          <video id="myPlayer" className="video-js vjs-default-skin" width={width} height={height}
-            controls preload="none" poster='/hamhama.jpg' 
+          <button id="ss" style={{ display: "none" }} className={videoStyle.yellowbutton}
+          >{choixX}</button>
+          <video id="myPlayer" className="video-js vjs-default-skin"
+            controls preload="none" poster='/hamhama.jpg'
             data-setup='{ 
            "responsive":true,
            "fluid":true,
@@ -105,36 +196,18 @@ export default function InteractiveVideo() {
         </div>
       </div>
 
+      <style jsx global>{`
+        particle {
+          position: fixed;
+          top: 0;
+          left: 0;
+          opacity: 0;
+          pointer-events: none;
+          background-repeat: no-repeat;
+          background-size: contain;
+        }
+      `}</style>
 
-
-    </>
+    </div>
   )
 }
-
-/*
-        <div className={videoStyle.textBox} >
-          <a href="#" className ={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate}`} onClick={(e) => executeChoixX(e)}>{choixX}</a>
-        </div>
-        */
-
-
-           /*
-      const videoJsOptions = { // lookup the options in the docs for more options
-        autoplay: false,
-        controls: false,
-        responsive: true,
-        fluid: true,
-      }
-    
-      // player is ready this will be executed
-      var player = videojs('myPlayer', videoJsOptions, (p) =>  {
-        videojs.log('Your player is ready!');
-    
-        // In this context, `this` is the player that was created by Video.js.
-        //this.play();
-      
-        // How about an event listener?
-        player.on('ended', function() {
-          draw()
-        });
-      });*/
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,85 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/InteractiveVideo.module.scss'
 import InteractiveVideo from '../components/InteractiveVideo'
-
+import '../styles/spinnerButton.module.scss'
 export default function Main() {
   return (
     <>
 
       <InteractiveVideo></InteractiveVideo>
+      <style jsx global>{`
+.video-js .vjs-control-bar {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 3em;
+  background: rgb(66,65,78);
+  background: linear-gradient(0deg, rgba(66,65,78,1) 0%, rgba(9,109,177,0) 100%);
+}
 
+.base-timer {
+  position: relative;
+  width: 50px;
+  height: 300px;
+  z-index: 1;
+}
+
+.base-timer__svg {
+  transform: scaleX(-1);
+}
+
+.base-timer__circle {
+  fill: none;
+  stroke: none;
+}
+
+.base-timer__path-elapsed {
+  stroke-width: 7px;
+  stroke: grey;
+}
+
+.base-timer__path-remaining {
+  stroke-width: 7px;
+  stroke-linecap: round;
+  transform: rotate(90deg);
+  transform-origin: center;
+  transition: 1s linear all;
+  fill-rule: nonzero;
+  stroke: currentColor;
+}
+
+.base-timer__path-remaining.green {
+  color: rgb(65, 184, 131);
+}
+
+.base-timer__path-remaining.orange {
+  color: orange;
+}
+
+.base-timer__path-remaining.red {
+  color: red;
+}
+
+.base-timer__label {
+  position: absolute;
+  width: 50px;
+
+  top: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 15px;
+}
+.video-js .vjs-progress-holder {
+  flex: auto;
+  -webkit-transition: all 0.2s;
+  transition: all 0.2s;
+  height: 0.3em;
+  display: none;
+}
+      `}</style>
     </>
   )
 }
