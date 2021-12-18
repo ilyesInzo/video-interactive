1152f19412090b0927d2abf952107dc6f7082948
user <ilyes.mansour.3@gmail.com>
2021-12-18 13:44:57 +0100

    cleanup code

--- components/InteractiveVideo.js
+++ components/InteractiveVideo.js
@@ -17,18 +17,8 @@ import scenaries from '../data/scenaries.json'
 //import anime from 'animejs/lib/anime.es.js';
 export default function InteractiveVideo() {
 
-
-  // normal enchainment
-  /*let scenarios = new Map([
-    ['choice1', 20],
-    ['choice2', 40],
-    ['choice3', 60],
-    ['choice4', 90],
-    ['choice5', 120],
-  ]
-  )*/
   // preceision en seconde
-  const precison = 0.5;
+  const precison = 0.3;
   let breackPointToScenarios = new Map()
 
   let player;
@@ -54,18 +44,6 @@ export default function InteractiveVideo() {
     currentChoix = choixY
   }
 
-  const runSpinner = (e) => {
-    displaySpinner()
-  }
-
-  const stopSpinner = (e) => {
-    hideSpinner()
-  }
-
-  const pauseSpinner = (e) => {
-    reverseSpinner()
-  }
-
   const getChoiceButton = (buttonId) => {
     return document.getElementById(buttonId)
   }
@@ -80,23 +58,12 @@ export default function InteractiveVideo() {
     currentAudio.play()
   }
 
-  const stopAudio = (audioId) => {
-    const currentAudio = getAudioComponent(audioId)
-    currentAudio.pause()
-    currentAudio.currentTime = 0;
-  }
-
   const getPlayer = () => videojs('myPlayer')
 
   useEffect(() => {
     player = getPlayer();
     //desintegrate("choixX")
     breackPointToScenarios = jsonToMap(setting())
-    /*console.log(breackPointToScenarios.get(10))*/
-    /*
-    runSpinner()
-    pauseSpinner()
-    */
     currentChoix = scenaries.intro
     var choixXBtn = getChoiceButton("choixX");
     var choixYBtn = getChoiceButton("choixY");
@@ -107,8 +74,8 @@ export default function InteractiveVideo() {
     choixXBtn.style.display = "none"
     choixYBtn.style.display = "none"
 
-
     fullScreenByElementIdHandler("go_fullscreen")
+    
     player.on('ready', () => {
       const bar = player.DisableProgressBar()
       bar.disable()
@@ -131,7 +98,7 @@ export default function InteractiveVideo() {
 
         breackPointToScenarios.forEach((value, key) => {
           if (((key - precison / 2) < currentTime) && (currentTime < (key + precison / 2))) {
-            console.log("errr");
+            console.log("Found");
             setChoixX(value.choix1)
             setChoixY(value.choix2)
             var choixXBtn = getChoiceButton("choixX");
@@ -153,13 +120,11 @@ export default function InteractiveVideo() {
       });
 
       player.on("pause", () => {
-        console.log("pause");
-        pauseSpinner()
+        reverseSpinner()
       });
 
       player.on("play", () => {
-        pauseSpinner()
-        console.log("play");
+        reverseSpinner()
       });
 
     });
@@ -171,21 +136,11 @@ export default function InteractiveVideo() {
 
       <audio id="clickSound" src="/audios/sound1.wav" type="audio/mpeg"></audio>
       <button id="go_fullscreen" style={{ display: "none" }}>Go FullScreen</button>
-      <button onClick={(e) => runSpinner(e)}>Run Spinner</button>
-      <button onClick={(e) => stopSpinner(e)}>Stop Spinner</button>
-      <button onClick={(e) => pauseSpinner(e)}>Pause Spinner</button>
       <div className={videoStyle.outerContainer}>
         <div id="container" className={videoStyle.innerContainer}>
-
           <button id="choixX" style={{ display: "none" }} className={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox}`} onClick={(e) => executeChoixX(e)}>{choixX.text}</button>
           <button id="choixY" style={{ display: "none" }} className={`${videoStyle.btn} ${videoStyle.btnWhite} ${videoStyle.btnAnimate} ${videoStyle.textBox2}`} onClick={(e) => executeChoixY(e)}>{choixY.text}</button>
-
           <div className={videoStyle.spinner} id="app"></div>
-          <button style={{ display: "none" }} className={videoStyle.videoOverlay} onClick={(e) => executeChoixX(e)}>{choixX.text}</button>
-          <button style={{ display: "none" }} className={videoStyle.videoOverlay2} onClick={(e) => executeChoixY(e)}>{choixY.text}</button>
-
-          <button id="ss" style={{ display: "none" }} className={videoStyle.yellowbutton}
-          >{choixX.text}</button>
           <video id="myPlayer" className="video-js vjs-default-skin"
             controls preload="none" poster='/hamhama.jpg'
             data-setup='{ 
